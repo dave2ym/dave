7d15888eb7835692c00fa1eb8bf0a9672ac9bfd7
dave2ym <dave2ym@gmail.com>
2020-06-09 22:27:28 +0100

    Nolock and Microsoft matching
    Added (nolock) on all selects and modified the match for MSSQL to 'Microsoft SQL Server 2___ %'

--- view1.sql
+++ view1.sql
@@ -3,6 +3,7 @@
 -- [AdminUsers] = [GSAAHSSRVWIN].[dbo].[AdminUsers]
 -- [DaveUsers] = [GSAAHSSRVWIN].[dbo].[QCheckUsers]
 
+
 select a.[Server], 
     softM.ProductName    as [MS_ProductName],
     softM.Version        as [MS_Version],
@@ -16,16 +17,17 @@ select a.[Server],
     c.[LogonTime] as AdminLogonTime, coalesce(c.[NoDays],999) as DaysPassedAdmin
 from
     (
-        select distinct [Server] from [GSAAHSSRVWIN].[dbo].[ActivityLog]
+        select distinct [Server] from [GSAAHSSRVWIN].[dbo].[ActivityLog] (nolock)
     ) a
     left join (
         select [Server], ProductName, Version, CreateDate, LastChangeDate, row_number() over(partition by [Server] order by [LastChangeDate] desc) as rank
-        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
-        where [ProductName] like '%Microsoft%SQL%'
+        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware] (nolock)
+        where [ProductName] like 'Microsoft SQL Server 2___ %'
+		
     ) softM on a.[Server] = softM.[Server] and softM.rank = 1
     left join (
         select [Server], ProductName, Version, CreateDate, LastChangeDate, row_number() over(partition by [Server] order by [LastChangeDate] desc) as rank
-        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
+        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware] (nolock)
         where [ProductName] like '%Oracle%'
     ) softO on a.[Server] = softO.[Server] and softO.rank = 1
     left join (
@@ -33,7 +35,7 @@ from
         from
         (
             select aa.*, row_number() over(partition by Server order by LogonTime desc) as rank
-            from [GSAAHSSRVWIN].[dbo].[ActivityLog] aa
+            from [GSAAHSSRVWIN].[dbo].[ActivityLog] (nolock) aa
                 left join [GSAAHSSRVWIN].[dbo].[AdminUsers] bb on aa.[User] = bb.[Username]
             where bb.[Username] is null
         ) z where rank = 1
@@ -43,7 +45,7 @@ from
         from
         (
             select aaa.*, row_number() over(partition by Server order by LogonTime desc) as rank
-            from [GSAAHSSRVWIN].[dbo].[ActivityLog] aaa
+            from [GSAAHSSRVWIN].[dbo].[ActivityLog] (nolock) aaa
                 inner join [GSAAHSSRVWIN].[dbo].[AdminUsers] bbb on aaa.[User] = bbb.[Username]
                 left join [GSAAHSSRVWIN].[dbo].[QCheckUsers] ccc on aaa.[User] = ccc.[Username]
             where ccc.[Username] is null
