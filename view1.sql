f826c15d36403ba17be41ccd9eb74d7d2d615aa5
I327776 <gabriel.tianu@sap.com>
2020-06-09 20:30:19 +0300

    versiunea pentru dave

--- view1.sql
+++ view1.sql
@@ -1,30 +1,34 @@
--- [ dormant] = [GSAAHSSRVWIN].[dbo].[ActivityLog]
--- [ software] = [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
--- [ AdminUsers] = [GSAAHSSRVWIN].[dbo].[AdminUsers]
--- [ DaveUsers] = [GSAAHSSRVWIN].[dbo].[QCheckUsers]
+-- [dormant] = [GSAAHSSRVWIN].[dbo].[ActivityLog]
+-- [software] = [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
+-- [AdminUsers] = [GSAAHSSRVWIN].[dbo].[AdminUsers]
+-- [DaveUsers] = [GSAAHSSRVWIN].[dbo].[QCheckUsers]
 
 select a.[Server], 
-    hasMsSql = case when soft.[hasMsSql] = 1 then 'true' else 'false' end,
-    hasOracle = case when soft.[hasOracle] = 1 then 'true' else 'false' end, b.[User], b.[Domain], b.[LogonTime], b.[CreateDate], coalesce(b.[NoDays],999) as DaysPassed,
+    hasMsSql = case when softM.[hasMsSql] = 1 then 'true' else 'false' end,
+    hasOracle = case when softO.[hasOracle] = 1 then 'true' else 'false' end,
+    b.[User], b.[Domain], b.[LogonTime], b.[CreateDate], coalesce(b.[NoDays],999) as DaysPassed,
     c.[LogonTime] as AdminLogonTime, coalesce(c.[NoDays],999) as DaysPassedAdmin
 from
     (
-        select distinct [Server] from [dormant]
+        select distinct [Server] from [GSAAHSSRVWIN].[dbo].[ActivityLog]
     ) a
     left join (
-        select [Server],
-            max(case when [ProductName] like '%Microsoft%SQL%' then 1 else null end) as [hasMsSql],
-            max(case when [ProductName] like '%Oracle%' then 1 else null end) as [hasOracle]
-        from [software]
-        group by [Server]
-    ) soft on a.[Server] = soft.[Server]
+        select distinct [Server], 1 as [hasMsSql]
+        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
+        where [ProductName] like '%Microsoft%SQL%'
+    ) softM on a.[Server] = softM.[Server]
+    left join (
+        select distinct [Server], 1 as [hasOracle]
+        from [GSAAHSSRVWIN].[dbo].[QCheckSoftware]
+        where [ProductName] like '%Oracle%'
+    ) softO on a.[Server] = softO.[Server]
     left join (
         select *, datediff(day, LogonTime, getdate()) as NoDays
         from
         (
             select aa.*, row_number() over(partition by Server order by LogonTime desc) as rank
-            from [dormant] aa
-                left join [AdminUsers] bb on aa.[User] = bb.[Username]
+            from [GSAAHSSRVWIN].[dbo].[ActivityLog] aa
+                left join [GSAAHSSRVWIN].[dbo].[AdminUsers] bb on aa.[User] = bb.[Username]
             where bb.[Username] is null
         ) z where rank = 1
     ) b on a.[Server] = b.[Server]
@@ -33,9 +37,9 @@ from
         from
         (
             select aaa.*, row_number() over(partition by Server order by LogonTime desc) as rank
-            from [dormant] aaa
-                inner join [AdminUsers] bbb on aaa.[User] = bbb.[Username]
-                left join [DaveUsers] ccc on aaa.[User] = ccc.[Username]
+            from [GSAAHSSRVWIN].[dbo].[ActivityLog] aaa
+                inner join [GSAAHSSRVWIN].[dbo].[AdminUsers] bbb on aaa.[User] = bbb.[Username]
+                left join [GSAAHSSRVWIN].[dbo].[QCheckUsers] ccc on aaa.[User] = ccc.[Username]
             where ccc.[Username] is null
         ) z where rank = 1
     ) c on a.[Server] = c.[Server] 
